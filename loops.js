6160604558b41148dbc324d56a6b5291340dad83
juancarlos240 <juancarlosromesanm@gmail.com>
2018-10-26 13:17:43 -0500

    Se suben variables y loops

---
+++ loops.js
@@ -0,0 +1,22 @@
+// while
+
+var text = " "
+var i = 0 
+ while (i < 5) {
+        text += "<br>The number is " + i;
+        i++;
+    }
+    console.log(text)
+
+
+
+// Do while
+
+var text = ""
+var i = 0;
+    do {
+        text += "<br>The number is " + i;
+        i++;
+    }
+    while (i < 5);
+    console.log(text)
---
+++ variables.js
@@ -0,0 +1,31 @@
+var nombre = "juan"
+var nombre = "Pedro"
+let nombre2 = "Chucho"
+
+if(true){
+   let nombre2 = "Mario"
+//   console.log("dentro if" + nombre2)
+}
+
+//  console.log(nombre2)
+
+
+const heroe = "Spiderman"
+nombre = "Pepe"
+
+nombre2 = "Julio"
+
+//heroe = "Vemon"
+
+//console.log(heroe)
+//console.log(nombre)
+//console.log(nombre2)
+
+if( 1 === "1"){
+   console.log("es verdadero")
+}
+
+for(let i = 0 ;i <=5 ; i++){
+      console.log(i)
+   }
+
